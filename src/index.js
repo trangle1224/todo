e2b55b6a3b0a264e35f6341c21623ed5fcc2d9dc
David Allen <davideallen7@gmail.com>
2018-07-09 12:47:51 -0700

    added api js file

---
+++ src/index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+
+const api = express();
+api.listen(3000, () => {
+  console.log('API up and running!');
+});
+
+api.get('/', (req, res) => {
+  console.log(req);
+  res.send('Hello, world!');
+});
