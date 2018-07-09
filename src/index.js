ae3422935c548f77cf08b375ad75d25552cb7b1c
David Allen <davideallen7@gmail.com>
2018-07-09 14:32:08 -0700

    serve todo list app

--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
 const express = require('express');
 
 const api = express();
+api.use(express.static(__dirname + '/public'));
+
+
 api.listen(3000, () => {
   console.log('API up and running!');
 });
 
-api.get('/', (req, res) => {
-  console.log(req);
-  res.send('Hello, world!');
-});
+// api.get('/', (req, res) => {
+//  console.log(req);
+//  res.send('Hello, world!');
+//});
